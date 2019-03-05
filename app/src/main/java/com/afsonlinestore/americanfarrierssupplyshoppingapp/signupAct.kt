b7d2ddd12e1f397d71d65415c4ee0cf1cd79af65
jack <mrjackroth5@gmail.com>
2019-03-05 14:06:44 +0800

    Sign Up View

---
+++ app/src/main/java/com/afsonlinestore/americanfarrierssupplyshoppingapp/signupAct.kt
@@ -0,0 +1,12 @@
+package com.afsonlinestore.americanfarrierssupplyshoppingapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class signupAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
